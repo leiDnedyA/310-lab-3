146eeea12a7d9b234b2349a7562bc3396879b059
leiDnedyA <aydendiel@gmail.com>
2024-05-09 17:22:53 -0400

    feat: added dp solution to q1

--- q1.py
+++ q1.py
@@ -6,14 +6,16 @@ def numSquares(n: int) -> int:
     Find the fewest number of perfect square numbers
     that can be added together to get n
     """
-    if n <= 3:
-        return n
-    result = n
-    for x in range(1, floor(sqrt(n)) + 1):
-        sq = x**2
-        potentialResult = 1 + numSquares(n - sq)
-        result = min(result, potentialResult)
-    return result
+    dp = [0, 1, 2, 3]
+    for i in range(4, n + 1):
+        dp.append(i)
+        for x in range(1, floor(sqrt(i)) + 1):
+            sq = x**2
+            if sq > i:
+                break
+            dp[i] = min(dp[i], 1 + dp[i - sq])
+    return dp[n]
+
     
 
 if __name__ == "__main__":
