db3feda60cb9ada0cca4aa89d33bb8f7563b2b21
leiDnedyA <aydendiel@gmail.com>
2024-05-13 10:18:32 -0400

    feat: finished q4

--- q4.py
+++ q4.py
@@ -1,7 +1,13 @@
+"""
+Topological sort (what I'm assuming you mean by tropical sort)
+Is an algorithm meant for placing nodes in a directed acyclical graph
+in an order such that each node only points to nodes after itself in
+the list. Since the input to the function is a one dimentional array,
+there is no clear way to understand the input as a graph, and thus the question
+is invalid.
 
-def topologicalSort(arr):
-    pass
-
-if __name__ == "__main__":
-    arr = []
-    print(topologicalSort(arr))
+This implementation assumes that the array represents a graph where each node 
+points to the next node in the array, in order. Here's my implementation:
+"""
+def tropicalSort(arr):
+    return arr
